9315b8f941d86ebe5cc73c73f4e1edbcf5ef6266
unknown <rtalaman@CDS-A04185-LT.fhcrc.org>
2014-04-10 22:13:21 +0100

    backup

---
+++ code-challenge/codeChallenge_Part3_userList/users/site/js/views/book.js
@@ -0,0 +1,26 @@
+// site/js/views/book.js
+
+var app = app || {};
+
+app.BookView = Backbone.View.extend({
+	
+	tagName: 'div',
+	className: 'bookContainer',
+	template: _.template( $('#bookTemplate').html() ),
+	
+	render: function() {
+		this.$el.html( this.template( this.model.toJSON ));
+		return this;
+	},
+	
+	events: {
+		'click .delete': 'deleteBook'
+	},
+	
+	deleteBook: function(){
+		// Delete model
+		this.model.destroy();
+		// Delete view
+		this.remove();
+	}
+});
